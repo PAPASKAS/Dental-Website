053060d9d38ec0973cea67038495490a7a499bfe
Pavel <Papaskaaaa@gmail.com>
2021-04-11 02:52:44 +0300

    our block
    Missed slider
    added our_block
    added demo mob ver our_block

--- Code/Home Page/javascript.js
+++ Code/Home Page/javascript.js
@@ -1,3 +1,27 @@
-function test_func() {
-    console.log("asdas");
+function func_like(){
+    var value = document.getElementById('like').src;
+    if(value == "http://127.0.0.1:5500/Source/Icons/like_off.svg"){
+        document.getElementById("like").src="../../Source/Icons/like_on.svg"
+    }
+    else{
+        document.getElementById("like").src="../../Source/Icons/like_off.svg"
+    }
+}
+function func_like2(){
+    var value2 = document.getElementById('like2').src;
+    if(value2 == "http://127.0.0.1:5500/Source/Icons/like_off.svg"){
+        document.getElementById("like2").src="../../Source/Icons/like_on.svg"
+    }
+    else{
+        document.getElementById("like2").src="../../Source/Icons/like_off.svg"
+    }
+}
+function func_like3(){
+    var value3 = document.getElementById('like3').src;
+    if(value3 == "http://127.0.0.1:5500/Source/Icons/like_off.svg"){
+        document.getElementById("like3").src="../../Source/Icons/like_on.svg"
+    }
+    else{
+        document.getElementById("like3").src="../../Source/Icons/like_off.svg"
+    }
 }
